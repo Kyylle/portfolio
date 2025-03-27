0b57bc773c8931d90762f643a4d5efc11ffa4880
Kyylle <kylekhezierdaugdaug@gmail.com>
2025-03-27 13:51:45 +0800

    Configure Nuxt for SSR and set base URL; enable prerendering for the homepage

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -6,11 +6,19 @@ export default defineNuxtConfig({
   css: ['@/assets/css/global.css'],
   plugins: ["~/plugins/fontawesome"],
 
+  ssr: false,
   app: {
     head: {
       title: "Portfolio",
     },
+    baseURL: '/portfolio/' // Replace with your GitHub repo name
   },
+  nitro: {
+    prerender: {
+      routes: ['/'] // Ensure the homepage is pre-rendered
+    }
+  },
+  
   runtimeConfig: {
     public: {
       apiBase: process.env.API_BASE_URL || "http://localhost:8000",
